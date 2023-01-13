77c877e81f0c504993435ae9ac24743f6f3d22c3
Esthernjoki <114407843+Esthernjoki@users.noreply.github.com>
2023-01-13 11:52:18 +0300

    first commit

---
+++ function-addThreeNumbers.js
@@ -0,0 +1,5 @@
+function addThreeNumbers(firstNumber,secondNumber,thirdNumber){
+    let sum=firstNumber+secondNumber+thirdNumber
+    console.log(sum)
+}
+function addThreeNumbers(5,3,2)
---
+++ function-divide.js
@@ -0,0 +1,6 @@
+function divideTwoNumbers(firstNumber,secondNumber){
+    let sum=firstNumber/secondNumber
+    console.log(sum)
+
+}
+divideTwoNumbers(18,6)
