b5ff138a24b303af8568039cf51cc930a819d743
Esthernjoki <114407843+Esthernjoki@users.noreply.github.com>
2023-01-14 05:13:07 +0300

    add.js

---
+++ function-add.js
@@ -0,0 +1,13 @@
+function addTwoNumbers(firstNumber, secondNumber){
+    let sum = firstNumber + secondNumber
+    console.log('Sum is: ', sum)
+}
+
+addTwoNumbers(4,5)
+
+
+function addTwoNumbers(firstNumber,secondNumber){
+    let sum=firstNumber+secondNumber
+    console.log('Sum is: ', sum)
+    }
+    addTwoNumbers(1,5)
