4656281bb1e08bb1a6bc6fa64a7ae44a741b9558
Esthernjoki <114407843+Esthernjoki@users.noreply.github.com>
2023-01-14 05:15:38 +0300

    addThreeNumbers

--- function-addThreeNumbers.js
+++ function-addThreeNumbers.js
@@ -2,4 +2,4 @@ function addThreeNumbers(firstNumber,secondNumber,thirdNumber){
     let sum=firstNumber+secondNumber+thirdNumber
     console.log(sum)
 }
-function addThreeNumbers(5,3,2)
+addThreeNumbers(5,3,2)
